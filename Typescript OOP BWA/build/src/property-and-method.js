9c8fe6b6b415534c3b1fa0e324a1e096187bb28d
nuhptr <nugrahaadi733@gmail.com>
2021-12-10 22:38:28 +0700

    update property and method

--- Typescript OOP BWA/build/src/property-and-method.js
+++ Typescript OOP BWA/build/src/property-and-method.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.people = void 0;
+exports.name = exports.people = void 0;
 // TODO: static method and static property
 class People {
-    constructor() {
+    constructor() { }
+    static getNameRole() {
+        return "hei";
     }
 }
 People.getRoleName = "Admin";
 exports.people = People.getRoleName;
-console.log(exports.people);
+console.log(exports.people); // Admin
+exports.name = People.getNameRole();
+console.log(exports.name); // hei
--- Typescript OOP BWA/src/property-and-method.ts
+++ Typescript OOP BWA/src/property-and-method.ts
@@ -3,7 +3,14 @@ class People {
     constructor() { }
 
     static getRoleName: string = "Admin";
+
+    static getNameRole() {
+        return "hei";
+    }
 }
 
 export let people = People.getRoleName;
 console.log(people); // Admin
+
+export let name = People.getNameRole();
+console.log(name); // hei
