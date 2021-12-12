9769c350ff69c74ab4fa7331bc9d51131f65a69f
nuhptr <nugrahaadi733@gmail.com>
2021-12-12 12:02:00 +0700

    delete build

--- Typescript OOP BWA/build/src/function-default-parameter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fullName = void 0;
-// TODO: default parameter
-const fullName = (firstName = "Adi", lastName = "Nugraha") => {
-    return firstName + " " + lastName;
-};
-exports.fullName = fullName;
-console.log((0, exports.fullName)("Abdi")); // => Putra Nugraha returnnya
