f157cd9a73be114b8b0c7cb5f156c6472bd49d30
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-12-26 20:28:42 +0700

    builder

---
+++ Design Patern/Creational/Builder/StudyCase.ts
@@ -0,0 +1,24 @@
+class User {
+  private name: string;
+  private username: string;
+  private password: string;
+
+  constructor(
+    name: string,
+    username: string,
+    password: string,
+    email: string,
+    address: string
+  ) {
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+}
+
+// TODO: bakalan ribet banget ngubah satu-satu di file manapun
+// const user1 = new User("adi", "nuhpt", "audirumah");
+// const user2 = new User("adi", "nuhpt", "audirumah");
+// const user3 = new User("adi", "nuhpt", "audirumah");
+// const user4 = new User("adi", "nuhpt", "audirumah");
+// const user5 = new User("adi", "nuhpt", "audirumah");
