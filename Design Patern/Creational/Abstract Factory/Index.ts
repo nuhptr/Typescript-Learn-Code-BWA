b64e338d0df86038d028d4064ee0905dc8af29fb
nuhptr <nugrahaadi733@gmail.com>
2021-12-27 13:30:30 +0700

    abstract factory

--- Design Patern/Creational/Abstract Factory/Index.ts
+++ Design Patern/Creational/Abstract Factory/Index.ts
@@ -1,99 +1,101 @@
-enum ComputerType {
-  PC = "pc",
-  LAPTOP = "laptop",
-}
-
-// Class contract
-abstract class Computer {
-  protected type: ComputerType;
-  protected monitor: string;
-
-  constructor(type: ComputerType, monitor: string) {
-    this.monitor = monitor;
-    this.type = type;
+namespace AbstractFactory {
+  enum ComputerType {
+    PC = "pc",
+    LAPTOP = "laptop",
   }
 
-  abstract getModel(): string;
-}
+  // Class contract
+  abstract class Computer {
+    protected type: ComputerType;
+    protected monitor: string;
 
-// jenis komputer PC
-class PC extends Computer {
-  constructor(monitor: string) {
-    super(ComputerType.PC, monitor);
+    constructor(type: ComputerType, monitor: string) {
+      this.monitor = monitor;
+      this.type = type;
+    }
+
+    abstract getModel(): string;
   }
 
-  getModel(): string {
-    return `${this.type} with ${this.monitor}`;
-  }
-}
+  // jenis komputer PC
+  class PC extends Computer {
+    constructor(monitor: string) {
+      super(ComputerType.PC, monitor);
+    }
 
-// jenis komputer laptop
-class Laptop extends Computer {
-  constructor(monitor: string) {
-    super(ComputerType.LAPTOP, monitor);
-  }
-
-  getModel(): string {
-    return `${this.type} and ${this.monitor}`;
-  }
-}
-
-enum MonitorType {
-  LED = "led",
-  IPS = "ips",
-}
-
-// class monitor LED
-class LEDComputerFactory {
-  static buildComputer(type: ComputerType): Computer {
-    switch (type) {
-      case ComputerType.PC:
-        return new PC(MonitorType.LED);
-      case ComputerType.LAPTOP:
-        return new Laptop(MonitorType.LED);
-      default:
-        throw new Error("type not found");
+    getModel(): string {
+      return `${this.type} with ${this.monitor}`;
     }
   }
-}
 
-// class monitor IPS
-class IPSComputerFactory {
-  static buildComputer(type: ComputerType): Computer {
-    switch (type) {
-      case ComputerType.PC:
-        return new PC(MonitorType.IPS);
-      case ComputerType.LAPTOP:
-        return new Laptop(MonitorType.IPS);
-      default:
-        throw new Error("type not found");
+  // jenis komputer laptop
+  class Laptop extends Computer {
+    constructor(monitor: string) {
+      super(ComputerType.LAPTOP, monitor);
+    }
+
+    getModel(): string {
+      return `${this.type} and ${this.monitor}`;
     }
   }
-}
 
-// Paling atas factory
-class ComputerFactory {
-  static buildComputer(
-    monitorType: MonitorType,
-    computerType: ComputerType
-  ): Computer {
-    switch (monitorType) {
-      case MonitorType.LED:
-        return LEDComputerFactory.buildComputer(computerType);
-      case MonitorType.IPS:
-        return IPSComputerFactory.buildComputer(computerType);
-      default:
-        throw new Error("type not found");
+  enum MonitorType {
+    LED = "led",
+    IPS = "ips",
+  }
+
+  // class monitor LED
+  class LEDComputerFactory {
+    static buildComputer(type: ComputerType): Computer {
+      switch (type) {
+        case ComputerType.PC:
+          return new PC(MonitorType.LED);
+        case ComputerType.LAPTOP:
+          return new Laptop(MonitorType.LED);
+        default:
+          throw new Error("type not found");
+      }
     }
   }
+
+  // class monitor IPS
+  class IPSComputerFactory {
+    static buildComputer(type: ComputerType): Computer {
+      switch (type) {
+        case ComputerType.PC:
+          return new PC(MonitorType.IPS);
+        case ComputerType.LAPTOP:
+          return new Laptop(MonitorType.IPS);
+        default:
+          throw new Error("type not found");
+      }
+    }
+  }
+
+  // Paling atas factory
+  class ComputerFactory {
+    static buildComputer(
+      monitorType: MonitorType,
+      computerType: ComputerType
+    ): Computer {
+      switch (monitorType) {
+        case MonitorType.LED:
+          return LEDComputerFactory.buildComputer(computerType);
+        case MonitorType.IPS:
+          return IPSComputerFactory.buildComputer(computerType);
+        default:
+          throw new Error("type not found");
+      }
+    }
+  }
+
+  // execute
+  const pcLed = ComputerFactory.buildComputer(MonitorType.LED, ComputerType.PC);
+  console.log(pcLed.getModel());
+
+  const laptopLed = ComputerFactory.buildComputer(
+    MonitorType.LED,
+    ComputerType.LAPTOP
+  );
+  console.log(laptopLed.getModel());
 }
-
-// execute
-const pcLed = ComputerFactory.buildComputer(MonitorType.LED, ComputerType.PC);
-console.log(pcLed.getModel());
-
-const laptopLed = ComputerFactory.buildComputer(
-  MonitorType.LED,
-  ComputerType.LAPTOP
-);
-console.log(laptopLed.getModel());
