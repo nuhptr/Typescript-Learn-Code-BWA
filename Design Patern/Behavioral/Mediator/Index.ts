8dcb4fc6bf09b7094253ceb1d8f9383919ade8fb
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-12-29 02:24:32 +0700

    update capital case

--- Design Patern/Behavioral/Mediator/Index.ts
+++ Design Patern/Behavioral/Mediator/Index.ts
@@ -5,86 +5,87 @@
  *
  *    penumpang1   penumpang2
  */
-
-interface IProduct {
-  name: string;
-  sell(): void;
-}
-
-interface IMediator {
-  registerProduct(product: Product): void;
-  setAvailableStatus(status: boolean): void;
-  isAvailable(): boolean;
-}
-
-class Product implements IProduct {
-  name: string;
-  Mediator: IMediator;
-
-  constructor(name: string, mediator: IMediator) {
-    this.name = name;
-    this.Mediator = mediator;
+namespace Mediator {
+  interface IProduct {
+    name: string;
+    sell(): void;
   }
 
-  sell(): void {
-    if (this.Mediator.isAvailable()) {
-      this.Mediator.setAvailableStatus(false);
-      console.log(`product ${this.name} berhasil dijual`);
-    } else {
-      console.log(
-        `product ini belum dijual, harus didaftarkan terlebih dahulu`
-      );
+  interface IMediator {
+    registerProduct(product: Product): void;
+    setAvailableStatus(status: boolean): void;
+    isAvailable(): boolean;
+  }
+
+  class Product implements IProduct {
+    name: string;
+    Mediator: IMediator;
+
+    constructor(name: string, mediator: IMediator) {
+      this.name = name;
+      this.Mediator = mediator;
     }
-  }
-}
 
-class ProductMediator implements IMediator {
-  product?: Product;
-  status: boolean = false;
-
-  registeredProduct(): void {
-    if (this.status) {
-      console.log(this.product);
-    } else {
-      console.log(`tidak ada product yang dijual`);
+    sell(): void {
+      if (this.Mediator.isAvailable()) {
+        this.Mediator.setAvailableStatus(false);
+        console.log(`product ${this.name} berhasil dijual`);
+      } else {
+        console.log(
+          `product ini belum dijual, harus didaftarkan terlebih dahulu`
+        );
+      }
     }
   }
 
-  registerProduct(product: Product): void {
-    if (this.status) {
-      console.log(
-        "tidak bisa mendaftarkan product, karena masih ada product yang belum terjual"
-      );
-    } else {
-      this.status = true;
-      this.product = product;
-      console.log("product berhasil di daftarkkan");
+  class ProductMediator implements IMediator {
+    product?: Product;
+    status: boolean = false;
+
+    registeredProduct(): void {
+      if (this.status) {
+        console.log(this.product);
+      } else {
+        console.log(`tidak ada product yang dijual`);
+      }
+    }
+
+    registerProduct(product: Product): void {
+      if (this.status) {
+        console.log(
+          "tidak bisa mendaftarkan product, karena masih ada product yang belum terjual"
+        );
+      } else {
+        this.status = true;
+        this.product = product;
+        console.log("product berhasil di daftarkkan");
+      }
+    }
+
+    setAvailableStatus(status: boolean): void {
+      this.status = status;
+    }
+
+    isAvailable(): boolean {
+      return this.status;
     }
   }
+  // execute
+  const mediator = new ProductMediator();
 
-  setAvailableStatus(status: boolean): void {
-    this.status = status;
-  }
+  const product1 = new Product("sabun", mediator);
+  const product2 = new Product("sampo", mediator);
 
-  isAvailable(): boolean {
-    return this.status;
-  }
+  mediator.registerProduct(product1);
+  mediator.registerProduct(product2);
+
+  product1.sell();
+  product2.sell();
+
+  mediator.registeredProduct();
+
+  mediator.registerProduct(product2);
+  mediator.registeredProduct();
+  product2.sell();
+  mediator.registeredProduct();
 }
-// execute
-const mediator = new ProductMediator();
-
-const product1 = new Product("sabun", mediator);
-const product2 = new Product("sampo", mediator);
-
-mediator.registerProduct(product1);
-mediator.registerProduct(product2);
-
-product1.sell();
-product2.sell();
-
-mediator.registeredProduct();
-
-mediator.registerProduct(product2);
-mediator.registeredProduct();
-product2.sell();
-mediator.registeredProduct();
