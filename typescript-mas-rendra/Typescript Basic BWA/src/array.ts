a527dfd214f8e8fb8a35b417363f1f3c37adfaa1
nuhptr <nugrahaadi733@gmail.com>
2023-02-28 23:23:58 +0700

    delete

--- typescript-mas-rendra/Typescript Basic BWA/src/array.ts
+++ typescript-mas-rendra/Typescript Basic BWA/src/array.ts
@@ -3,11 +3,17 @@ let array: number[];
 array = [1, 2, 3];
 
 let array2: string[];
-array2 = ['adi', 'nugraha', 'putra'];
+array2 = ["adi", "nugraha", "putra"];
 
 let array3: any[];
-array3 = [1, 'String', true];
+array3 = [1, "String", true];
 
 console.log(array);
 console.log(array2);
 console.log(array3);
+
+/* 
+[ 1, 2, 3 ]
+[ "adi", "nugraha", "putra" ]
+[ 1, "String", true ] 
+*/
--- typescript-mas-rendra/Typescript Basic BWA/src/enum.ts
+++ typescript-mas-rendra/Typescript Basic BWA/src/enum.ts
@@ -20,3 +20,28 @@ export enum Day {
 
 console.table(Month);
 console.table(Day);
+
+/* 
+───────┬────────┐
+│ (idx) │ Values │
+├───────┼────────┤
+│ 1     │ "JAN"  │
+│ 2     │ "FEB"  │
+│ 3     │ "MAR"  │
+│ 4     │ "APR"  │
+│ 5     │ "MEI"  │
+│ JAN   │ 1      │
+│ FEB   │ 2      │
+│ MAR   │ 3      │
+│ APR   │ 4      │
+│ MEI   │ 5      │
+└───────┴────────┘
+┌───────┬─────────────┐
+│ (idx) │ Values      │
+├───────┼─────────────┤
+│ MON   │ "Monday"    │
+│ TUE   │ "Tuesday"   │
+│ WED   │ "Wednesday" │
+│ THR   │ "Thursday"  │
+│ FRI   │ "Friday"    │ 
+*/
