b191b33df044f260c3f81c73c31b40d30137a4cf
nuhptr <nugrahaadi733@gmail.com>
2023-12-04 00:15:02 +0700

    optional-properties/update code

---
+++ adi-explore/type-aliases/optional-properties.ts
@@ -0,0 +1,14 @@
+type Forest = {
+   name: string
+   mount: number
+   readonly location?: string // optional property and read-only
+   trees?: string[] // optional property
+}
+
+const forest: Forest = {
+   name: "Amazon",
+   mount: 1000,
+   trees: ["Oak", "Pine"],
+}
+
+console.log(forest.name, forest.mount, forest.trees ?? ["No trees"])
