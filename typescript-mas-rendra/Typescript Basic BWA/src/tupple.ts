6f478ba9c73a581a086d7f7cae294f51ea7aa542
nuhptr <nugrahaadi733@gmail.com>
2023-12-03 20:17:09 +0700

    node_modules/ignore

--- typescript-mas-rendra/Typescript Basic BWA/src/tupple.ts
+++
@@ -1,5 +0,0 @@
-// TODO: tupple -> 2 typedata to allow assigned data
-let biodata: [string, number]
-
-biodata = ["Surabaya", 20]
-console.log(biodata) // ["Surabaya", 20]
---
+++ typescript-mas-rendra/todo-express/src/routers/UserRouter.ts
@@ -0,0 +1,17 @@
+import UserController from "../controllers/UserController"
+import BaseRouter from "./BaseRouter"
+import { auth } from "../middlewares/AuthMiddleware"
+
+class UserRouter extends BaseRouter {
+   // TODO: data dummy
+   public routes(): void {
+      this.router.get("/", auth, UserController.index)
+      this.router.post("/", UserController.create)
+      this.router.get("/:id", UserController.show)
+      this.router.put("/:id", UserController.update)
+      this.router.delete("/:id", UserController.delete)
+   }
+}
+
+// TODO : setiap selesai membuat
+export default new UserRouter().router
