868962d4293d665fb2ac2deab28a9788b504238a
nuhptr <nugrahaadi733@gmail.com>
2023-12-04 00:14:34 +0700

    inference-types/update code

---
+++ adi-explore/inference-types/index.ts
@@ -0,0 +1,7 @@
+// Auto inferring types
+// Detect the type of the variable based on the value assigned to it
+let tech = "Typescript"
+let favNumber = 8
+let tsHard = false
+
+console.info(tech, favNumber, tsHard)
