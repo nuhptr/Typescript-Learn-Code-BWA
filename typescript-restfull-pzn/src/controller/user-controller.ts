d38e5fd883a0f5a9db2e612b8af43407ca63b699
nuhptr <nugrahaadi733@gmail.com>
2024-03-07 13:44:57 +0700

    controller/add user controller

---
+++ typescript-restfull-pzn/src/controller/user-controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express"
+
+import { CreateUserRequest, LoginUserRequest } from "../model/user-model"
+import { UserService } from "../services/user-service"
+import { UserRequest } from "../type/user-request"
+
+export class UserController {
+   static async register(req: Request, res: Response, next: NextFunction) {
+      try {
+         const request: CreateUserRequest = req.body as CreateUserRequest
+         const response = await UserService.register(request)
+         res.status(201).json({ data: response })
+      } catch (error) {
+         next(error)
+      }
+   }
+
+   static async login(req: Request, res: Response, next: NextFunction) {
+      try {
+         const request: LoginUserRequest = req.body as LoginUserRequest
+         const response = await UserService.login(request)
+         res.status(200).json({ data: response })
+      } catch (error) {
+         next(error)
+      }
+   }
+
+   static async get(req: UserRequest, res: Response, next: NextFunction) {
+      try {
+         const response = await UserService.get(req.user!)
+         res.status(200).json({ data: response })
+      } catch (error) {
+         next(error)
+      }
+   }
+}
