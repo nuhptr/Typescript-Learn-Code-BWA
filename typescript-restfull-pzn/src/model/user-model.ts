4202811292832d1bda8b6407be709a07e2c333a3
nuhptr <nugrahaadi733@gmail.com>
2024-03-07 13:44:10 +0700

    model/add user model

---
+++ typescript-restfull-pzn/src/model/user-model.ts
@@ -0,0 +1,30 @@
+/**
+ * User Model
+ * This is a validate user type model
+ */
+
+import { User } from "@prisma/client"
+
+export type UserResponse = {
+   name: string
+   username: string
+   token?: string
+}
+
+export type CreateUserRequest = {
+   name: string
+   username: string
+   password: string
+}
+
+export type LoginUserRequest = {
+   username: string
+   password: string
+}
+
+export function toUserResponse(user: User): UserResponse {
+   return {
+      name: user.name,
+      username: user.username,
+   }
+}
