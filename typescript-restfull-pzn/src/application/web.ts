b15b481453a606a8ea61281fab7d771a1149d7fe
nuhptr <nugrahaadi733@gmail.com>
2024-03-08 15:18:10 +0700

    web/add new package use

--- typescript-restfull-pzn/src/application/web.ts
+++ typescript-restfull-pzn/src/application/web.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import helmet from "helmet"
 
 import { publicRouter } from "../route/public-api"
 import { apiRouter } from "../route/api"
@@ -6,6 +7,7 @@ import { errorMiddleware } from "../middleware/error-middleware"
 
 export const web = express()
 web.use(express.json())
+web.use(helmet())
 web.use(publicRouter)
 web.use(apiRouter)
 web.use(errorMiddleware)
