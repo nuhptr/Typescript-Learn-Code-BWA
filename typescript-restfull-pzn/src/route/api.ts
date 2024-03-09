0bb6e84f0caa9f67d97263274577d01876c2f303
nuhptr <nugrahaadi733@gmail.com>
2024-03-10 00:00:39 +0700

    route: add contact to private api

--- typescript-restfull-pzn/src/route/api.ts
+++ typescript-restfull-pzn/src/route/api.ts
@@ -1,7 +1,8 @@
 import express from "express"
 
-import { authMiddleware } from "../middleware/auth-middleware"
-import { UserController } from "../controller/user-controller"
+import { authMiddleware } from "@/middleware/auth-middleware"
+import { UserController } from "@/controller/user-controller"
+import { ContactController } from "@/controller/contact-controller"
 
 export const apiRouter = express.Router()
 apiRouter.use(authMiddleware)
@@ -10,3 +11,8 @@ apiRouter.use(authMiddleware)
 apiRouter.get("/api/users/current", UserController.get)
 apiRouter.patch("/api/users/current", UserController.update)
 apiRouter.delete("/api/users/current", UserController.logout)
+
+// Contacts API
+apiRouter.post("/api/contacts", ContactController.create)
+// :contactId(\\d+) is a regex to ensure contactId is a number
+apiRouter.get("/api/contacts/:contactId(\\d+)", ContactController.get)
