ea0d69cca98b5532fb34842398ede70b2837cacf
nuhptr <nugrahaadi733@gmail.com>
2024-03-09 23:58:20 +0700

    service: add contact service

---
+++ typescript-restfull-pzn/src/services/contact-service.ts
@@ -0,0 +1,39 @@
+import { User } from "@prisma/client"
+
+import { ContactResponse, toContactResponse, CreateContactRequest } from "@/model/contact-model"
+import { ContactValidation } from "@/validation/contact-validation"
+import { Validation } from "@/validation/validation"
+import { prismaClient } from "@/application/database"
+import { ResponseError } from "@/error/response-error"
+
+export class ContactService {
+   static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
+      const createRequest = Validation.validate(ContactValidation.CREATE, request)
+
+      const record = {
+         ...{ username: user.username },
+         ...createRequest,
+      }
+
+      const contact = await prismaClient.contact.create({
+         data: record,
+      })
+
+      return toContactResponse(contact)
+   }
+
+   static async get(user: User, id: number): Promise<ContactResponse> {
+      const contact = await prismaClient.contact.findUnique({
+         where: {
+            id: id,
+            username: user.username,
+         },
+      })
+
+      if (!contact) {
+         throw new ResponseError(404, "Contact not found")
+      }
+
+      return toContactResponse(contact)
+   }
+}
