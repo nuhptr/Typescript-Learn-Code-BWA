d4eee7db1bd3d60e19b7e12229044a015ae6a52a
nuhptr <nugrahaadi733@gmail.com>
2024-03-10 00:01:07 +0700

    update import

--- typescript-restfull-pzn/src/application/web.ts
+++ typescript-restfull-pzn/src/application/web.ts
@@ -1,9 +1,9 @@
 import express from "express"
 import helmet from "helmet"
 
-import { publicRouter } from "../route/public-api"
-import { apiRouter } from "../route/api"
-import { errorMiddleware } from "../middleware/error-middleware"
+import { publicRouter } from "@/route/public-api"
+import { apiRouter } from "@/route/api"
+import { errorMiddleware } from "@/middleware/error-middleware"
 
 export const web = express()
 web.use(express.json())
