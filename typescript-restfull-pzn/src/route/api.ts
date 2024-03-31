75bffe121db410612c7f5521ab84a8977a5e3ada
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:46:20 +0700

    api: add update and remove route

--- typescript-restfull-pzn/src/route/api.ts
+++ typescript-restfull-pzn/src/route/api.ts
@@ -16,3 +16,5 @@ apiRouter.delete("/api/users/current", UserController.logout)
 apiRouter.post("/api/contacts", ContactController.create)
 // :contactId(\\d+) is a regex to ensure contactId is a number
 apiRouter.get("/api/contacts/:contactId(\\d+)", ContactController.get)
+apiRouter.put("/api/contacts/:contactId(\\d+)", ContactController.update)
+apiRouter.delete("/api/contacts/:contactId(\\d+)", ContactController.remove)
