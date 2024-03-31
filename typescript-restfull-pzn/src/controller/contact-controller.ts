649883716c7e00dbee36d3eb206fd256b0e10af2
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:46:44 +0700

    contact: update model, controller, service, validation, and test

--- typescript-restfull-pzn/src/controller/contact-controller.ts
+++ typescript-restfull-pzn/src/controller/contact-controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express"
 
 import { UserRequest } from "@/type/user-request"
 import { ContactService } from "@/services/contact-service"
-import { CreateContactRequest } from "@/model/contact-model"
+import { CreateContactRequest, UpdateContactRequest } from "@/model/contact-model"
 import { logger } from "@/application/logging"
 
 export class ContactController {
@@ -10,6 +10,7 @@ export class ContactController {
       try {
          const request: CreateContactRequest = req.body as CreateContactRequest
          const response = await ContactService.create(req.user!, request)
+         logger.debug("response : " + JSON.stringify(response))
          res.status(201).json({ data: response })
       } catch (error) {
          next(error)
@@ -22,6 +23,31 @@ export class ContactController {
          const response = await ContactService.get(req.user!, contactId)
          logger.debug("response : " + JSON.stringify(response))
          res.status(200).json({ data: response })
+      } catch (error) {
+         next(error)
+      }
+   }
+
+   static async update(req: UserRequest, res: Response, next: NextFunction) {
+      try {
+         const request: UpdateContactRequest = req.body as UpdateContactRequest
+         request.id = Number(req.params.contactId)
+
+         const response = await ContactService.update(req.user!, request)
+         logger.debug("response : " + JSON.stringify(response))
+         res.status(200).json({ data: response })
+      } catch (error) {
+         next(error)
+      }
+   }
+
+   static async remove(req: UserRequest, res: Response, next: NextFunction) {
+      try {
+         const contactId = Number(req.params.contactId)
+         const response = await ContactService.remove(req.user!, contactId)
+
+         logger.debug("response : " + JSON.stringify(response))
+         res.status(200).json({ data: "OK" })
       } catch (error) {
          next(error)
       }
--- typescript-restfull-pzn/test/contact.test.ts
+++ typescript-restfull-pzn/test/contact.test.ts
@@ -82,3 +82,81 @@ describe("GET /api/contacts/:contactId", () => {
       expect(response.body.errors).toBeDefined()
    })
 })
+
+// PUT /api/contacts/:contactId
+describe("PUT /api/contacts/:contactId", () => {
+   beforeEach(async () => {
+      await UserTest.create()
+      await ContactTest.create()
+   })
+
+   afterEach(async () => {
+      await UserTest.delete()
+      await ContactTest.deleteAll()
+   })
+
+   it("should be able update contact", async () => {
+      const contact = await ContactTest.get()
+      const response = await supertest(web).put(`/api/contacts/${contact.id}`).set("X-API-TOKEN", "test").send({
+         first_name: "John",
+         last_name: "Doe",
+         email: "jhondoe@example.com",
+         phone: "081234567890",
+      })
+
+      logger.debug(response.body)
+      expect(response.status).toBe(200)
+      expect(response.body.data.id).toBe(contact.id)
+      expect(response.body.data.first_name).toBe("John")
+      expect(response.body.data.last_name).toBe("Doe")
+      expect(response.body.data.email).toBe("jhondoe@example.com")
+      expect(response.body.data.phone).toBe("081234567890")
+   })
+
+   it("should reject update contact if request is invalid", async () => {
+      const contact = await ContactTest.get()
+      const response = await supertest(web).put(`/api/contacts/${contact.id}`).set("X-API-TOKEN", "test").send({
+         first_name: "",
+         last_name: "",
+         email: "jhondoe",
+         phone: "",
+      })
+
+      logger.debug(response.body)
+      expect(response.status).toBe(400)
+      expect(response.body.errors).toBeDefined()
+   })
+})
+
+// DELETE /api/contacts/:contactId
+describe("DELETE /api/contacts/:contactId", () => {
+   beforeEach(async () => {
+      await UserTest.create()
+      await ContactTest.create()
+   })
+
+   afterEach(async () => {
+      await UserTest.delete()
+      await ContactTest.deleteAll()
+   })
+
+   it("should be able delete contact", async () => {
+      const contact = await ContactTest.get()
+      const response = await supertest(web).delete(`/api/contacts/${contact.id}`).set("X-API-TOKEN", "test")
+
+      logger.debug(response.body)
+      expect(response.status).toBe(200)
+      expect(response.body.data).toBe("OK")
+   })
+
+   it("should reject delete contact if contact is not found", async () => {
+      const contact = await ContactTest.get()
+      const response = await supertest(web)
+         .delete(`/api/contacts/${contact.id + 1}`)
+         .set("X-API-TOKEN", "test")
+
+      logger.debug(response.body)
+      expect(response.status).toBe(404)
+      expect(response.body.errors).toBeDefined()
+   })
+})
