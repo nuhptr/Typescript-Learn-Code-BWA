758c8a5ea3e3ef01060782d9e9d5bbefdbc4d14a
nuhptr <nugrahaadi733@gmail.com>
2024-04-01 17:14:23 +0700

    test: add utilites function

--- typescript-restfull-pzn/test/test-util.ts
+++ typescript-restfull-pzn/test/test-util.ts
@@ -1,15 +1,13 @@
 import bcrypt from "bcrypt"
+import { Address, Contact, User } from "@prisma/client"
 
 import { prismaClient } from "@/application/database"
-import { Contact, User } from "@prisma/client"
 
 export class UserTest {
     static async delete() {
         // using deleteMany whatever data exist or not it still running delete
         await prismaClient.user.deleteMany({
-            where: {
-                username: "test",
-            },
+            where: { username: "test" },
         })
     }
 
@@ -26,14 +24,10 @@ export class UserTest {
 
     static async get(): Promise<User> {
         const user = await prismaClient.user.findFirst({
-            where: {
-                username: "test",
-            },
+            where: { username: "test" },
         })
 
-        if (!user) {
-            throw new Error("User not found.")
-        }
+        if (!user) throw new Error("User not found.")
 
         return user
     }
@@ -42,9 +36,7 @@ export class UserTest {
 export class ContactTest {
     static async deleteAll() {
         await prismaClient.contact.deleteMany({
-            where: {
-                username: "test",
-            },
+            where: { username: "test" },
         })
     }
 
@@ -65,10 +57,46 @@ export class ContactTest {
             where: { username: "test" },
         })
 
-        if (!contact) {
-            throw new Error("Contact not found.")
-        }
+        if (!contact) throw new Error("Contact not found.")
 
         return contact
+    }
+}
+
+export class AddressTest {
+    static async deleteAll() {
+        await prismaClient.address.deleteMany({
+            where: {
+                contact: {
+                    username: "test",
+                },
+            },
+        })
+    }
+
+    static async create() {
+        const contact = await ContactTest.get()
+        await prismaClient.address.create({
+            data: {
+                contact_id: contact.id,
+                street: "Jl. Test",
+                city: "Jakarta",
+                province: "DKI Jakarta",
+                country: "Indonesia",
+                postal_code: "12345",
+            },
+        })
+    }
+
+    static async get(): Promise<Address> {
+        const address = await prismaClient.address.findFirst({
+            where: {
+                contact: { username: "test" },
+            },
+        })
+
+        if (!address) throw new Error("Address not found.")
+
+        return address
     }
 }
