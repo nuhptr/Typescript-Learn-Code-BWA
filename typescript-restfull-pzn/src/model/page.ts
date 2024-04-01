eb006c1910bd63edd1e1c1af55df81059d550f89
nuhptr <nugrahaadi733@gmail.com>
2024-04-01 17:10:29 +0700

    model: page add description comment

--- typescript-restfull-pzn/src/model/page.ts
+++ typescript-restfull-pzn/src/model/page.ts
@@ -4,6 +4,7 @@ export type Paging = {
     current_page: number
 }
 
+//* Pageable is a generic type that has two properties: data and paging. The data property is an array of T, while the paging property is a Paging type.
 export type Pageable<T> = {
     data: Array<T>
     paging: Paging
