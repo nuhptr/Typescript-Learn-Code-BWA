5cc6d8004a75a0f1afe4210cf91af6a50fb6387f
nuhptr <nugrahaadi733@gmail.com>
2024-04-02 17:00:25 +0700

    controller: add remove and list controller also change to relative path

--- typescript-restfull-pzn/src/controller/adress-controller.ts
+++ typescript-restfull-pzn/src/controller/adress-controller.ts
@@ -1,8 +1,13 @@
 import { Response, NextFunction } from "express"
 
-import { UserRequest } from "@/type/user-request"
-import { CreateAdressRequest, GetAddressRequest, updateAddressRequest } from "@/model/address-model"
-import { AddressService } from "@/services/address-service"
+import { UserRequest } from "../type/user-request"
+import {
+    CreateAdressRequest,
+    GetAddressRequest,
+    RemoveAddressRequest,
+    updateAddressRequest,
+} from "../model/address-model"
+import { AddressService } from "../services/address-service"
 
 export class AdressController {
     static async create(req: UserRequest, res: Response, next: NextFunction) {
@@ -42,6 +47,30 @@ export class AdressController {
             request.id = Number(req.params.addressId)
 
             const response = await AddressService.update(req.user!, request)
+            res.status(200).json({ data: response })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async remove(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const request: RemoveAddressRequest = {
+                id: Number(req.params.addressId),
+                contact_id: Number(req.params.contactId),
+            }
+
+            await AddressService.remove(req.user!, request)
+            res.status(200).json({ data: "OK" })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async list(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const contactId = Number(req.params.contactId)
+            const response = await AddressService.list(req.user!, contactId)
             res.status(200).json({ data: response })
         } catch (error) {
             next(error)
