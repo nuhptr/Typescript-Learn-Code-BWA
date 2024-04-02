8a141e9571241e524358e74eaca8e1c3b6e57113
nuhptr <nugrahaadi733@gmail.com>
2024-04-02 17:39:41 +0700

    service: address add remove, list, and change positioning of get

--- typescript-restfull-pzn/src/services/address-service.ts
+++ typescript-restfull-pzn/src/services/address-service.ts
@@ -1,17 +1,18 @@
 import { Address, User } from "@prisma/client"
 
-import { prismaClient } from "@/application/database"
-import { ContactService } from "@/services/contact-service"
+import { prismaClient } from "../application/database"
+import { ContactService } from "../services/contact-service"
 import {
     AddressResponse,
     CreateAdressRequest,
     GetAddressRequest,
+    RemoveAddressRequest,
     toAddressResponse,
     updateAddressRequest,
-} from "@/model/address-model"
-import { Validation } from "@/validation/validation"
-import { AddressValidation } from "@/validation/address-validation"
-import { ResponseError } from "@/error/response-error"
+} from "../model/address-model"
+import { Validation } from "../validation/validation"
+import { AddressValidation } from "../validation/address-validation"
+import { ResponseError } from "../error/response-error"
 
 export class AddressService {
     static async create(user: User, request: CreateAdressRequest): Promise<AddressResponse> {
@@ -21,17 +22,6 @@ export class AddressService {
         const address = await prismaClient.address.create({
             data: createRequest as any,
         })
-
-        return toAddressResponse(address)
-    }
-
-    static async get(user: User, request: GetAddressRequest): Promise<AddressResponse> {
-        const getRequest = Validation.validate(AddressValidation.GET, request)
-        await ContactService.checkContactMustExist(user.username, request.contact_id)
-
-        const address = await this.checkAddressMustExist(getRequest.contact_id, getRequest.id)
-
-        if (!address) throw new ResponseError(404, "Address not found")
 
         return toAddressResponse(address)
     }
@@ -50,6 +40,16 @@ export class AddressService {
         return address
     }
 
+    static async get(user: User, request: GetAddressRequest): Promise<AddressResponse> {
+        const getRequest = Validation.validate(AddressValidation.GET, request)
+        await ContactService.checkContactMustExist(user.username, request.contact_id)
+        const address = await this.checkAddressMustExist(getRequest.contact_id, getRequest.id)
+
+        if (!address) throw new ResponseError(404, "Address not found")
+
+        return toAddressResponse(address)
+    }
+
     static async update(user: User, request: updateAddressRequest): Promise<AddressResponse> {
         const updateRequest = Validation.validate(AddressValidation.UPDATE, request)
         await ContactService.checkContactMustExist(user.username, request.contact_id)
@@ -64,5 +64,34 @@ export class AddressService {
         })
 
         return toAddressResponse(address)
+    }
+
+    static async remove(user: User, request: RemoveAddressRequest): Promise<AddressResponse> {
+        const removeRequest = Validation.validate(AddressValidation.REMOVE, request)
+        await ContactService.checkContactMustExist(user.username, request.contact_id)
+        const checkAddress = await this.checkAddressMustExist(
+            removeRequest.contact_id,
+            removeRequest.id
+        )
+
+        if (!checkAddress) throw new ResponseError(404, "Address not found")
+
+        const address = await prismaClient.address.delete({
+            where: {
+                id: removeRequest.id,
+            },
+        })
+
+        return toAddressResponse(address)
+    }
+
+    static async list(user: User, contactId: number): Promise<Array<AddressResponse>> {
+        await ContactService.checkContactMustExist(user.username, contactId)
+
+        const addresses = await prismaClient.address.findMany({
+            where: { contact_id: contactId },
+        })
+
+        return addresses.map((address) => toAddressResponse(address))
     }
 }
