57cb9eb4a44b9455e7ffa138feec77f34080edc6
nuhptr <nugrahaadi733@gmail.com>
2024-04-02 17:44:35 +0700

    test: address update, get, delete data test

--- typescript-restfull-pzn/test/address.test.ts
+++ typescript-restfull-pzn/test/address.test.ts
@@ -137,7 +137,7 @@ describe("GET /api/contacts/:contactId/addresses/:addressId", function () {
 })
 
 // PUT /api/contacts/:contactId/addresses/:addressId
-describe("PUT /api/contacts/:contactId/addresses/:addressId", () => {
+describe("PUT /api/contacts/:contactId/addresses/:addressId", function () {
     beforeEach(async function () {
         await UserTest.create()
         await ContactTest.create()
@@ -209,6 +209,119 @@ describe("PUT /api/contacts/:contactId/addresses/:addressId", () => {
                 country: "Indonesia",
                 postal_code: "12345",
             })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(404)
+        expect(response.body.errors).toBeDefined()
+    })
+
+    it("should reject to update address if contact is not found", async function () {
+        const contact = await ContactTest.get()
+        const address = await AddressTest.get()
+
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id + 1}/addresses/${address.id}`)
+            .set("X-API-TOKEN", "test")
+            .send({
+                street: "Jalan belum ada",
+                city: "Jakarta",
+                province: "DKI Jakarta",
+                country: "Indonesia",
+                postal_code: "12345",
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(404)
+        expect(response.body.errors).toBeDefined()
+    })
+})
+
+// DELETE /api/contacts/:contactId/addresses/:addressId
+describe("DELETE /api/contacts/:contactId/addresses/:addressId", function () {
+    beforeEach(async function () {
+        await UserTest.create()
+        await ContactTest.create()
+        await AddressTest.create()
+    })
+
+    afterEach(async function () {
+        await AddressTest.deleteAll()
+        await ContactTest.deleteAll()
+        await UserTest.delete()
+    })
+
+    it("should be able to remove address", async function () {
+        const contact = await ContactTest.get()
+        const address = await AddressTest.get()
+
+        const response = await supertest(web)
+            .delete(`/api/contacts/${contact.id}/addresses/${address.id}`)
+            .set("X-API-TOKEN", "test")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBe("OK")
+    })
+
+    it("should reject to remove address if address is not found", async function () {
+        const contact = await ContactTest.get()
+        const address = await AddressTest.get()
+
+        const response = await supertest(web)
+            .delete(`/api/contacts/${contact.id}/addresses/${address.id + 1}`)
+            .set("X-API-TOKEN", "test")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(404)
+        expect(response.body.errors).toBeDefined()
+    })
+
+    it("should reject to remove address if contact is not found", async function () {
+        const contact = await ContactTest.get()
+        const address = await AddressTest.get()
+
+        const response = await supertest(web)
+            .delete(`/api/contacts/${contact.id + 1}/addresses/${address.id}`)
+            .set("X-API-TOKEN", "test")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(404)
+        expect(response.body.errors).toBeDefined()
+    })
+})
+
+// GET /api/contacts/:contactId/addresses
+describe("GET /api/contacts/:contactId/addresses", function () {
+    beforeEach(async function () {
+        await UserTest.create()
+        await ContactTest.create()
+        await AddressTest.create()
+    })
+
+    afterEach(async function () {
+        await AddressTest.deleteAll()
+        await ContactTest.deleteAll()
+        await UserTest.delete()
+    })
+
+    it("should be able to get list address", async function () {
+        const contact = await ContactTest.get()
+
+        const response = await supertest(web)
+            .get(`/api/contacts/${contact.id}/addresses`)
+            .set("X-API-TOKEN", "test")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body.data.length).toBe(1)
+    })
+
+    it("should reject list address if contact is not found", async function () {
+        const contact = await ContactTest.get()
+
+        const response = await supertest(web)
+            .get(`/api/contacts/${contact.id + 1}/addresses`)
+            .set("X-API-TOKEN", "test")
 
         logger.debug(response.body)
         expect(response.status).toBe(404)
